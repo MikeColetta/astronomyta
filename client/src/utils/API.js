04f227b5c62f45c76163f055c7756ceee40cb172
Mike Coletta <coletta.mike@gmail.com>
2021-05-22 13:53:27 -0700

    general astronomy search for homepage images

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,6 +8,9 @@ export default {
     //Grabs a book from the Google API
     getNASAAPOD: function() {
         return axios.get(`https://api.nasa.gov/planetary/apod?api_key=${REACT_APP_NASA_API}`)
+    },
+    getNASAAstronomy: function() {
+        return axios.get(`https://images-api.nasa.gov/search?q=astronomy`)
     },    
     getNASAPlanets: function() {
         return axios.get(`https://images-api.nasa.gov/search?q=planets`)
