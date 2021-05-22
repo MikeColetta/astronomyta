3977ce814eff1728a39ca6858c8578b63c6bb4a8
Colin Bell <cobell206@gmail.com>
2021-05-22 11:16:37 -0700

    added comment and post component

---
+++ client/src/components/Comment/index.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react"
+import React, { useState, useEffect } from React
+import Card from 'react-bootstrap'
+
+function Comment(props) {
+
+    const [data, setData] = useState()
+
+    useEffect(() => {
+
+    })
+
+
+
+    return (
+        <Card>
+            <Card.Header>{props.user}</Card.Header>
+            <Card.Body>
+                <Card.Text className='text-muted'>{props.date}</Card.Text>
+                <Card.Text>{props.comment}</Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Comment
