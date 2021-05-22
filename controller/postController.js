68b6c160929030692a28ac3f0876dac73de10449
Colin Bell <cobell206@gmail.com>
2021-05-22 14:40:05 -0700

    updated routes and controller

--- controller/postController.js
+++ controller/postController.js
@@ -1,1 +1,23 @@
 const db = require('../models')
+
+module.exports = {
+    findAll: function(req, res) {
+        db.Post
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    findById: function(req, res) {
+        db.Post
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    create: function(req, res) {
+        db.Post
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    }
+}
