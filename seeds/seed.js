259fd8deab508aed09df3bdb83004a32dae9f0f4
Mike Coletta <coletta.mike@gmail.com>
2021-05-25 21:34:11 -0700

    Updated seed file with users

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,46 @@
 const User = require('../models/users');
-const Post = require('../models/Post');
+// const Post = require('../models/Post');
 const mongoose = require('mongoose');
+
+const users = [
+    {
+        name: 'Parm',
+        email: 'parm@mail.com',
+        password: 'password123'
+    },
+    {
+        name: 'Mike',
+        email: 'mike@mail.com',
+        password: 'password123'   
+    },
+    {
+        name: 'Debasu',
+        email: 'debasu@mail.com',
+        password: 'password123'   
+    },
+    {
+        name: 'Haram',
+        email: 'haram@mail.com',
+        password: 'password123'   
+    },
+    {
+        name: 'Patrick',
+        email: 'patrick@mail.com',
+        password: 'password123'   
+    },
+    {
+        name: 'Colin',
+        email: 'colin@mail.com',
+        password: 'password123'   
+    },
+]
 
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/astronomyta",
-    function () {
-        mongoose.connection.db.dropDatabase();
-    },
+    // function () {
+    //     mongoose.connection.db.dropDatabase();
+    // },
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -16,22 +49,21 @@ mongoose.connect(
     }
 );
 
-const mikePost = {
-    text: 'New Post!',
-    imageLink: 'https://images-assets.nasa.gov/image/8003541/8003541~thumb.jpg',
-    description: 'High Energy Astronomy Observatory (HEAO)'
-}
+// const mikePost = {
+//     text: 'New Post!',
+//     imageLink: 'https://images-assets.nasa.gov/image/8003541/8003541~thumb.jpg',
+//     description: 'High Energy Astronomy Observatory (HEAO)'
+// }
 
-const mikeUser = {
-    name: 'Mike',
-    email: 'mike@mail.com',
-    password: 'password123'
-}
 
-User.create(mikeUser)
-    .then(res => console.log(res))
+
+User.create(users)
+    .then(res => {
+        console.log(`Users added ${res}`)
+        process.exit(0)
+    })
     .catch(err => console.error(err))
 
-Post.create(mikePost)
-     .then(res => console.log(res))
-     .catch(err => console.error(err))
+// Post.create(mikePost)
+//      .then(res => console.log(res))
+//      .catch(err => console.error(err))
