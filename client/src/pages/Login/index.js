197cd758af636978ad26f73c1fe9da0180e929eb
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-25 19:53:18 -0700

    Google button is placed below the login & signup.

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Loginpic from '../../assets/images/Loginpic.jpg';
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Loginpic from "../../assets/images/Loginpic.jpg";
 
 function Profile({ authService }) {
   const onLogin = (event) => {
@@ -18,75 +18,69 @@ function Profile({ authService }) {
 
   return (
     <div>
-      <section>
-        <section>
-          <h1>Login</h1>
-          <ul>
-            <li>
-              <button onClick={onLogin}>Google</button>
-            </li>
-          </ul>
-        </section>
-      </section>
       <Jumbotron
-      className="jumbotronStyle"
+        className="jumbotronStyle"
         style={{ backgroundImage: `url(${Loginpic})` }}
       />
       <Container>
         <Row>
-          <Card className='m-3' style={{ width: '25rem' }}>
+          <Card className="m-3" style={{ width: "25rem" }}>
             <Card.Header>Login</Card.Header>
-            <Form className='m-3'>
-              <Form.Group controlId='formBasicEmail'>
+            <Form className="m-3">
+              <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email Adress</Form.Label>
                 <Form.Control
-                  type='email'
-                  placeholder='Enter Email'
+                  type="email"
+                  placeholder="Enter Email"
                 ></Form.Control>
               </Form.Group>
-              <Form.Group controlId='formBasicPassword'>
+              <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
-                  type='password'
-                  placeholder='Password'
+                  type="password"
+                  placeholder="Password"
                 ></Form.Control>
               </Form.Group>
-              <Button variant='primary' type='submit'>
+              <Button variant="primary" type="submit">
                 Login
               </Button>
             </Form>
           </Card>
 
-          <Card className='m-3' style={{ width: '25rem' }}>
+          <Card className="m-3" style={{ width: "25rem" }}>
             <Card.Header>Create Account</Card.Header>
-            <Form className='m-3'>
-              <Form.Group controlId='formBasicName'>
+            <Form className="m-3">
+              <Form.Group controlId="formBasicName">
                 <Form.Label>Name</Form.Label>
                 <Form.Control
-                  type='name'
-                  placeholder='Enter Name'
+                  type="name"
+                  placeholder="Enter Name"
                 ></Form.Control>
               </Form.Group>
-              <Form.Group controlId='formBasicEmail'>
+              <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email Adress</Form.Label>
                 <Form.Control
-                  type='email'
-                  placeholder='Enter Email'
+                  type="email"
+                  placeholder="Enter Email"
                 ></Form.Control>
               </Form.Group>
-              <Form.Group controlId='formBasicPassword'>
+              <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
-                  type='password'
-                  placeholder='Password'
+                  type="password"
+                  placeholder="Password"
                 ></Form.Control>
               </Form.Group>
-              <Button variant='primary' type='submit'>
+              <Button variant="primary" type="submit">
                 Sign Up
               </Button>
             </Form>
           </Card>
         </Row>
+        <section>
+          <h1>Login</h1>
+          <button onClick={onLogin}>Google</button>
+        </section>
       </Container>
     </div>
   );
