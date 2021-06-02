66d360cd21d589d3e03a874f61a486141bab874c
Mike Coletta <coletta.mike@gmail.com>
2021-06-01 19:36:34 -0700

    Added sorted results to topic pages for saved posts

--- client/src/pages/Asteroids/index.js
+++ client/src/pages/Asteroids/index.js
@@ -18,15 +18,15 @@ function Asteroids() {
   const [likedPosts, setLikedPosts] = useState([]);
 
   useEffect(() => {
-    API.getAllPosts()
-    .then((res) => {
-      let sortedPosts = res.data.sort((a, b) => b.likes - a.likes )
-      setLikedPosts(sortedPosts)
-    })
-    .catch((err) => console.error(err))
+    API.getAllPosts('Asteroids')
+      .then((res) => {
+        let sortedPosts = res.data.sort((a, b) => b.likes - a.likes)
+        setLikedPosts(sortedPosts)
+      })
+      .catch((err) => console.error(err))
 
     API.getNASAAsteroids()
-      .then(res => setAsteroidPhotos(res.data.collection.items.slice(0, 5).map(postData => createPost(postData, 'Asteroids'))))
+      .then(res => setAsteroidPhotos(res.data.collection.items.slice(0, 5).map(postData => createPost(postData))))
       .catch(err => console.error(err))
 
     API.getYouTubeAstroids()
@@ -36,7 +36,7 @@ function Asteroids() {
       .catch(err => console.error(err))
   }, [])
 
-  
+
   return (
     <div className="pageStyle">
       <Jumbotron
@@ -47,33 +47,39 @@ function Asteroids() {
       </Jumbotron>
       <Container>
         <Row>
-          
-            <p className="descriptionText">
-              Asteroids, sometimes called minor planets, are rocky, airless
-              remnants left over from the early formation of our solar system
-              about 4.6 billion years ago. The current known asteroid count is:
-              1,081,541. Most of this ancient space rubble can be found orbiting
-              the Sun between Mars and Jupiter within the main asteroid belt.
-              Asteroids range in size from Vesta — the largest at about 329
-              miles (530 kilometers) in diameter — to bodies that are less than
-              33 feet (10 meters) across. The total mass of all the asteroids
-              combined is less than that of Earth's Moon.
+
+          <p className="descriptionText">
+            Asteroids, sometimes called minor planets, are rocky, airless
+            remnants left over from the early formation of our solar system
+            about 4.6 billion years ago. The current known asteroid count is:
+            1,081,541. Most of this ancient space rubble can be found orbiting
+            the Sun between Mars and Jupiter within the main asteroid belt.
+            Asteroids range in size from Vesta — the largest at about 329
+            miles (530 kilometers) in diameter — to bodies that are less than
+            33 feet (10 meters) across. The total mass of all the asteroids
+            combined is less than that of Earth's Moon.
             </p>
-          
+
         </Row>
         <Row>
           <Col>
             <Card>
               <ListGroup>
+                {likedPosts.map((post) => {
+                  return <Post props={post} isSaved={true}></Post>;
+                })}
                 {asteroidsYT.map((photo) => {
-                    return <Post props={photo} isSaved={false}></Post>
-                  })} 
+                  return <Post props={photo} isSaved={false}></Post>
+                })}
               </ListGroup>
             </Card>
           </Col>
           <Col>
             <Card>
               <ListGroup>
+                {likedPosts.map((post) => {
+                  return <Post props={post} isSaved={true}></Post>;
+                })}
                 {asteroidPhotos.map((photo) => {
                   return <Post props={photo} isSaved={false}></Post>
                 })}
