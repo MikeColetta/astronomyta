ac7578c3dfc7e09416934e61fafefbd725ce7715
Mike Coletta <coletta.mike@gmail.com>
2021-06-03 21:14:28 -0700

    Comment component loads date and text

--- client/src/components/Comment/index.js
+++ client/src/components/Comment/index.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import {Card} from 'react-bootstrap'
+import './style.css'
+import moment from 'moment';
 
 function Comment(props) {
 
     const [data, setData] = useState()
 
     useEffect(() => {
+        console.log('loggin comment')
         console.log(props)
     })
-
-
 
     return (
         <Card>
             <Card.Body>
-                {/* <Card.Text className='text-muted'>{props.props.date}</Card.Text> */}
-                <Card.Text>{props.text}</Card.Text>
+                <Card.Text className='text-muted'>{moment(props.props.date).format('MM/DD/YYYY')}</Card.Text>
+                <Card.Text className='commentText'>{props.props.text}</Card.Text>
             </Card.Body>
         </Card>
     )
